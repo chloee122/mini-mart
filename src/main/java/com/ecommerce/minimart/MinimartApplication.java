db9cc1d2151e2b3dc86291efda5a00d74b000253
Cinnamon Bun <nguyeney02@gmail.com>
2024-12-30 18:51:36 +0200

    initial commit

---
+++ src/main/java/com/ecommerce/minimart/MinimartApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.minimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimartApplication.class, args);
+	}
+
+}
